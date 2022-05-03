b8ebe918ec372252d3f7cfb271b928c086ed09cd
1SizeS1 <65680700+1SizeS1@users.noreply.github.com>
2022-05-04 01:39:32 +0300

    Add files via upload

---
+++ src/configs/config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    OwnerID: "801069133810237491", //BOT OWNER
+    Owners: ["968156734214508584"], //SUNUCU OWNER
+    GuildID: "924987418002718771", //SUNUCU ID
+    VoiceID: "968115020779102258", //SES ODASI ID
+
+    Token: "OTcxMTU5Nzk0NzUwNzMwMzIx.YnGdCg.wGwKRQcjSUKsVPdEUnJ5Yt7dte0", //BOT TOKEN
+    MongoIRL: "mongodb+srv://harlexg:harlexg@cluster0.7sbmg.mongodb.net/harlexunity?retryWrites=true&w=majority", //MONGO URL
+    Prefixs: ["."], //PREFIXLER
+
+    Tag: "§", //SUNUCU TAGINIZ
+    SecondaryTag: "§", //DOKUNMASANIZDA OLUR
+
+    Status: "dnd", // dnd - online - idle
+    Message: "Harlex ❤️ quDev", //BOT DURUMU
+    Footer: "Harlex ❤️ quDev" //setFooter
+}
